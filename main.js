82341dbff34b7526bda1282430db93fb0b0321e4
Antonio <antonioboksic1603@gmail.com>
2023-05-17 01:51:30 +0200

    provato a fare anche con ciclo for semplice invece che con forEach, non riesco a gestire i bottoni all'interno dei cicli

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const posts = [
     {
         "id": 1,
@@ -56,10 +57,12 @@ const posts = [
     }
 ];
 
-const container = document.getElementById("container") 
+const elContainer = document.getElementById("container") 
 
-console.log(container)
+console.log(elContainer)
 
+// forEach non mi funziona quando devo selezionare i 6 bottoni, me ne seleziona solo uno
+/*
 posts.forEach(element => {
     const elPost = `<div class="post">
 
@@ -84,7 +87,7 @@ posts.forEach(element => {
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <a class="like-button  js-like-button" href="#" id="likeButton" data-postid="${element.id}">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
@@ -95,9 +98,141 @@ posts.forEach(element => {
         </div> 
     </div> 
 
-</div>`;
+    </div>`;
 
-container.innerHTML += elPost
+    //aggiungo il template literal elPost nel container
+    elContainer.innerHTML += elPost
 
+    console.log(element.likes)
     
+
+    //identifico il bottone in HTML 
+    const elLikeButton = document.getElementById("likeButton")
+
+    console.log(elLikeButton)
+
+    elLikeButton.addEventListener("click",
+        function() {
+            elLikeButton.classList.add("liked");
+            element.likes = 50 ;
+            //return element.likes
+            console.log(element.likes)
+            
+        }
+    )
+ 
 });
+*/
+
+//provo con for normale
+
+
+
+for (i = 0 ; i < posts.length; i++) {
+    const elPost = `<div class="post">
+
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src=${posts[i].author.image} alt=${posts[i].author.name}>                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts[i].author.name}</div>
+                <div class="post-meta__time">${posts[i].created}</div>
+            </div>                    
+        </div>
+    </div>
+
+    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+    
+    <div class="post__image">
+        <img src=${posts[i].media} alt="">
+    </div>
+
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" id="likeButton" data-postid="${posts[i].id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+            </div>
+        </div> 
+    </div> 
+
+    </div>`;
+
+    //aggiungo il template literal elPost nel container
+    elContainer.innerHTML += elPost
+
+    //identifico il bottone in HTML 
+    const elLikeButton = document.querySelector("#likeButton")
+    
+
+    console.log(elLikeButton)
+
+    elLikeButton.addEventListener("click",
+    function() {
+        elLikeButton.classList.add("liked");
+        posts[i].likes = 50 ;
+        //return posts[i].likes
+        console.log(posts[i].likes)
+        
+    }
+)
+
+}
+
+
+
+
+
+// // OGGETTO SINGOLO
+// const palla = {
+//     "colore": "rosso",
+//     "diametro": 50
+// }
+
+// //cambio proprietà
+// palla.colore = "verde"
+// //aggiungo proprietà
+// palla.edition = "special"
+// //cambio proprietà
+// palla.diametro = palla.diametro+1
+// //console.log(palla)
+
+// // ARRAY DI OGGETTI
+
+
+
+
+
+
+/*
+
+const arrPalle = [
+    {
+    "colore": "rosso",
+    "diametro": 50
+
+    },
+
+    {
+        "colore": "blu",
+        "diametro": 22
+
+    }
+]
+
+console.log(arrPalle)
+
+arrPalle.forEach(element => {
+    element.diametro += 1
+    return element.diametro
+});
+
+console.log(arrPalle)
+*/
